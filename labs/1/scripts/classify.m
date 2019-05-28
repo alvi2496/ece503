fdd1c00ef9d5d4b7315bacb4a01acfb82d6f5294
Alvi Mahadi <alvi.utsab@gmail.com>
2019-05-28 16:29:28 -0700

    lab works

--- labs/1/scripts/classify.m
+++ labs/1/scripts/classify.m
@@ -1,24 +1,26 @@
-function result = classify(T, l, U, mew, F)
-    result = 0;
-    for i=1:100
-        x = T(:, i);
-        d = 123456.1234;
-        for j=1:10
-           Uq_j = U(:, (j-1)*29+1:j*29);
-           Fj = F(:, (j-1)*1600+1:j*1600);
-           mew_j = mew(:, j);
-           x_head = Uq_j * Fj + mew_j;
-           ej = norm((x-x_head), 2);
-           if (ej < d) 
-               d = ej;
-               class = j-1;
-           end
-        end
-        if (l(i) == class)
-            result = result + 1;
-        end
-        fprintf("Image: %d; Actual: %d, Prediction: %d\n", i,class,l(i));
+function [correct, incorrect] = classify(T, l, U, mew)
+    correct = 0;
+    incorrect = 0;
+    chunk = 1000;
+    for k = 1:chunk:10000
+       time_before = datetime('now');
+       for i = k:k+chunk-1
+            x = T(:, i);
+            ej = double.empty;
+            for j=1:10
+               Uq_j = U(:, (j-1)*29+1:j*29);
+               mew_j = mew(:, j);
+               Fj = Uq_j' * (x - mew_j);
+               x_head = Uq_j * Fj + mew_j;
+               ej = [ej norm(x - x_head)];
+            end
+            [~, class] = min(ej);
+            if (l(i) == class-1)
+                correct = correct + 1;
+            else
+                incorrect = incorrect + 1;
+            end
+       end
+       fprintf("CPU time of %d - %d: %s\n", k, i, datetime('now') - time_before);
     end
-    result = result / (i-1) * 100;
 end
-
--- labs/1/scripts/pca.m
+++ labs/1/scripts/pca.m
@@ -1,15 +1,13 @@
-function [mew, U, F] = pca(f)
+function [mew, U] = pca(f)
     mew = double.empty;
     U = double.empty;
-    F = double.empty;
-    [m, n] = size(f);
-    for i = 1:1600:n
-       Xj = f(1:m, i:i+1599);
-       mew = [mew mean(Xj')'];
-       A = Xj - mew(int16(i / 1600) + 1);
-       [Uj, Sj] = eigs(((A * A') / 1600), 29);
+    chunk = 1600;
+    for i = 1:chunk:16000
+       Xj = f(:, i:i+chunk-1);
+       mew = [mew mean((Xj'))'];
+       A = Xj - mew(:, int16(i / 1600) + 1);
+       [Uj, ~] = eigs(((A * A') / 1600), 29);
        U = [U Uj];
-       F = [F (Uj' * Xj)];
     end
 end
 
