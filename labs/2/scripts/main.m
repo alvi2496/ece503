837e2049904c7ba27b92e3dd0e59a21c0f672bd4
Alvi Mahadi <alvi.utsab@gmail.com>
2019-06-11 15:56:03 -0700

    complete lab 2

--- labs/2/scripts/main.m
+++ labs/2/scripts/main.m
@@ -1,9 +1,8 @@
 clc;
 clear all;
 close all;
-D = load('/home/alvi/Documents/courses/ece503/assignments/2/D_iris.mat');
-D = D.D_iris;
-D = D(1:4,:);
+D = load('D_iris.mat');
+D = D.D_iris(1:4,:);
 X1 = D(:,1:50);
 X2 = D(:,51:100);
 X3 = D(:,101:150);
@@ -19,4 +18,39 @@ rand('state',113)
 r3 = randperm(50);
 Xtr3 = X3(:,r3(1:40));
 Xte3 = X3(:,r3(41:50));
-Dtrain = [Xtr1 Xtr2 Xtr3];
+y = [ones(40, 1); -ones(80, 1)];
+ones = ones(120, 1);
+% 3.3 (i) calculate w1
+Dtrain1 = [Xtr1'; Xtr2'; Xtr3'];
+Dtrain1 = [Dtrain1 ones];
+w1 = (inv(Dtrain1' * Dtrain1) * Dtrain1') * y;
+% 3.3 (ii) calculate w2
+Dtrain2 = [Xtr2'; Xtr1'; Xtr3'];
+Dtrain2 = [Dtrain2 ones];
+w2 = (inv(Dtrain2' * Dtrain2) * Dtrain2') * y;
+% 3.3 (iii) calculate w3
+Dtrain3 = [Xtr3'; Xtr2'; Xtr1'];
+Dtrain3 = [Dtrain3 ones];
+w3 = (inv(Dtrain3' * Dtrain3) * Dtrain3') * y;
+mis_class = 0;
+Yte1(1, 1:10) = 1;
+Yte2(1, 1:10) = 2;
+Yte3(1, 1:10) = 3;
+Xtest = [Xte1 Xte2 Xte3];
+Ytest = [Yte1 Yte2 Yte3];
+Y = [1 0 0; 0 1 0; 0 0 1];
+Yresult = int16.empty;
+for i=1:30
+   f1 = (w1(1:4, :)' * Xtest(:, i) + w1(5));
+   f2 = (w2(1:4, :)' * Xtest(:, i) + w2(5));
+   f3 = (w3(1:4, :)' * Xtest(:, i) + w3(5));
+   F = [f1 f2 f3];
+   [~, I] = max(F);
+   Yresult = [Yresult Y(I, :)'];
+   if I ~= Ytest(i)
+       mis_class = mis_class + 1;
+   end
+end
+error_rate = mis_class / 30;
+disp(Yresult);
+disp(error_rate);
