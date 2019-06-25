2b08a91ec29c4bda06ad82482a1cec29dc84ca15
Alvi Mahadi <alvi.utsab@gmail.com>
2019-06-25 15:05:00 -0700

    lab 3 script

--- labs/3/scripts/main.m
+++ labs/3/scripts/main.m
@@ -8,4 +8,18 @@ D_te = load('/home/alvi/Documents/courses/ece503/labs/3/data/D_build_te.mat');
 Xte = D_te.D_build_te(1:8,:);
 Yte = D_te.D_build_te(9:10, :);
 Xtr = [Xtr' ones(640, 1)];
-W = (inv(Xtr' * Xtr) * Xtr') * Ytr';
+I = eye(9);
+W_B = pinv(Xtr' * Xtr + 0.01 * I) * Xtr' * Ytr';
+W = [W_B(1:8, 1) W_B(1:8, 2)];
+b = W_B(9, :)';
+e_p = double.empty();
+Y = double.empty();
+for i=1:128
+    y = W' * Xte(:, i) + b;
+    Y = [Y, y];
+end
+e_p = norm(Yte - Y, 'fro') / norm(Yte, 'fro');
+disp(e_p);
+I = 1:128;
+plot(I, Yte(1, I), 'r', I, Y(1, I), 'g');
+plot(I, Yte(2, I), 'r', I, Y(2, I), 'g');
